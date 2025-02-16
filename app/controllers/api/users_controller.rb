a5ee0af2be83267cbf4fadec6cbd17f884b15857
vanganur <bharathi.vanganuru@xfel.eu>
2025-02-16 19:21:17 +0100

    api fix done

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,19 @@
-class Api::UsersController < ApplicationController
-  def index
-    users = User.all
-    render json: users
-  end
+class Api::UsersController < Api::ApplicationController
+
+  before_action :set_user, only: [:update]
 
   def update
-    user = User.find(params[:id])
-    user.update(active: params[:active], updated_at: Time.current)
-    render json: user
+    if @user.update(active: params[:active])
+      render json: { message: "User updated successfully", user: @user }, status: :ok
+    else
+      render json: { error: "Failed to update user" }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+    render json: { error: "User not found" }, status: :not_found unless @user
   end
 end
